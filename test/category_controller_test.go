1e982da7554a8087b44b289189536800e15af279
Meizaluna Wulandari <MeizalunaWulandari@gmail.com>
2023-05-07 23:39:16 +0800

    Unit Test - Test Create Category Failed

--- test/category_controller_test.go
+++ test/category_controller_test.go
@@ -67,3 +67,26 @@ func TestCreateCategorySuccess(t *testing.T) {
 	assert.Equal(t, "OK", responseBody["status"])
 	assert.Equal(t, "Gadget Test", responseBody["data"].(map[string]interface{})["name"])
 }
+
+func TestCreateCategoryFailed(t *testing.T) {
+	db := setupTestDB()
+	TruncateCategory(db)
+	router := setupRouter(db)
+	requestBody := strings.NewReader(`{"name": ""}`)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8000/api/categories", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	assert.Equal(t, 400, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 400, int(responseBody["code"].(float64)))
+	assert.Equal(t, "BAD REQUEST", responseBody["status"])
+
+}
