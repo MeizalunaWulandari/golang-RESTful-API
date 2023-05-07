d30b9b3b88941f6f51903122adc6636b22066d9e
Meizaluna Wulandari <MeizalunaWulandari@gmail.com>
2023-05-07 23:24:30 +0800

    Unit Test - Test Create Category Success

--- test/category_controller_test.go
+++ test/category_controller_test.go
@@ -2,20 +2,27 @@ package test
 
 import (
 	"database/sql"
+	"encoding/json"
 	"golang-resful-api/app"
 	"golang-resful-api/controller"
 	"golang-resful-api/helper"
 	"golang-resful-api/middleware"
 	"golang-resful-api/repository"
 	"golang-resful-api/service"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 	"time"
 
 	"github.com/go-playground/validator"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
 )
 
 func setupTestDB() *sql.DB {
-	db, err := sql.Open("mysql", "root@tcp(localhost:3360)/golang_resful_api_test")
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/golang_resful_api_test")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -25,12 +32,38 @@ func setupTestDB() *sql.DB {
 	return db
 }
 
-func setupRouter() http.Handler {
-	db := setupTestDB()
+func setupRouter(db *sql.DB) http.Handler {
 	validate := validator.New()
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 	router := app.NewRouter(categoryController)
 	return middleware.NewAuthMiddleware(router)
+}
+
+func TruncateCategory(db *sql.DB) {
+	db.Exec("TRUNCATE category")
+}
+
+func TestCreateCategorySuccess(t *testing.T) {
+	db := setupTestDB()
+	TruncateCategory(db)
+	router := setupRouter(db)
+	requestBody := strings.NewReader(`{"name": "Gadget Test"}`)
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8000/api/categories", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
+	assert.Equal(t, "Gadget Test", responseBody["data"].(map[string]interface{})["name"])
 }
