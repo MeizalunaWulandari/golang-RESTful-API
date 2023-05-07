12e5289ee8919a92486dc51bce3ff7b524d986eb
Meizaluna Wulandari <MeizalunaWulandari@gmail.com>
2023-05-07 14:44:26 +0800

    Unit Test - Setup Database

--- test/category_controller_test.go
+++ test/category_controller_test.go
@@ -1,6 +1,7 @@
-package main
+package test
 
 import (
+	"database/sql"
 	"golang-resful-api/app"
 	"golang-resful-api/controller"
 	"golang-resful-api/helper"
@@ -8,25 +9,28 @@ import (
 	"golang-resful-api/repository"
 	"golang-resful-api/service"
 	"net/http"
+	"time"
 
 	"github.com/go-playground/validator"
-	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	db := app.NewDB()
+func setupTestDB() *sql.DB {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3360)/golang_resful_api_test")
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+	return db
+}
+
+func setupRouter() http.Handler {
+	db := setupTestDB()
 	validate := validator.New()
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
-
 	router := app.NewRouter(categoryController)
-
-	server := http.Server{
-		Addr: "localhost:8000",
-
-		Handler: middleware.NewAuthMiddleware(router),
-	}
-	err := server.ListenAndServe()
-	helper.PanicIfError(err)
+	return middleware.NewAuthMiddleware(router)
 }
